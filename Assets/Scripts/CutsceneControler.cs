5196e63de9fe090e2ab7b421f4a736bf0f447ab7
bbrdbr <aubreysp2015@gmail.com>
2026-02-01 06:44:22 -0400

    implemented dialogue to cutscene controller; suffered

--- Assets/Scripts/CutsceneControler.cs
+++ Assets/Scripts/CutsceneControler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Assets.Scripts.CustomDebug;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.Profiling;
 
 
 // This class will be a parent class for other cutscenes
@@ -22,9 +23,10 @@ public class CutsceneController : MonoBehaviour
     [SerializeField] private Canvas canvas;
     [SerializeField] private GameObject CutsceneMouseIcon;
     [SerializeField] private Image image;
-
+    [SerializeField] private Image dialogueImage;
+    private bool isPlayingDialogue = false;
     public bool timeout;
-
+    private bool firstTime = true;
 
     void Awake()
     {
@@ -43,16 +45,19 @@ public class CutsceneController : MonoBehaviour
     void Start()
     {
         CutsceneMouseIcon.SetActive(false);
-       
+        dialogueImage.gameObject.SetActive(false);
+        dialogueImage.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+        DebugStatsDisplay.Instance.RegisterDebugStatsRequest(new DebugStatsRequest("First Time", () => {return firstTime;}));
+        DebugStatsDisplay.Instance.RegisterDebugStatsRequest(new DebugStatsRequest("timeout", () => {return timeout;}));
     }
 
     void Update()
     {
-        if(cutsceneType == CutSceneType.playerWalkThrough && !timeout && !CutsceneMouseIcon.activeSelf)
+        if(cutsceneType == CutSceneType.playerWalkThrough && !timeout && !isPlayingDialogue && !CutsceneMouseIcon.activeSelf)
         {
-            // enable the mouse Icon
             CutsceneMouseIcon.SetActive(true);
         }
+
     }
 
     public void StartCutscene()
@@ -77,16 +82,30 @@ public class CutsceneController : MonoBehaviour
 
     public void AdvanceCutscene()
     {
-        Debug.Log(currentSprite);
+        // If dialogue is playing or we’re past the last sprite, do nothing
+        if (currentSprite >= SpriteList.Count || isPlayingDialogue)
+            return;
+
         SpriteRecord currRecord = SpriteList[currentSprite];
+
+        // Show main slide immediately
         image.sprite = currRecord.sprite;
 
-        currentSprite++;
-
-        timeout = true;
-        CutsceneMouseIcon.SetActive(false);
-        StartCoroutine(UnTimeout(currRecord.viewtime));
+        if (currRecord.hasDialogue && currRecord.dialogues.Count > 0)
+        {
+            if (firstTime)
+            {
+                firstTime = false;
+                return;
+            }
+            StartCoroutine(PlayDialogue(currRecord.dialogues, currRecord.viewtime));
+        }
+        else
+        {
+            StartCoroutine(AdvanceScene(currRecord.viewtime));
+        }
     }
+
 
     IEnumerator PlayCutscene()
     {
@@ -104,15 +123,94 @@ public class CutsceneController : MonoBehaviour
         // EndCutscene();
     }
 
+    private IEnumerator PlayDialogue(List<SpriteRecord.Dialogue> dialogues, int mainSlideTime)
+    {
+        bool clicked;
+        isPlayingDialogue = true;
+        dialogueImage.gameObject.SetActive(true);
+
+        // Wait a frame to avoid leftover clicks skipping the first dialogue
+        yield return null;
+
+        foreach (var dialogue in dialogues)
+        {
+            dialogueImage.sprite = dialogue.dialogueSprite;
+            dialogueImage.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+            // Step 1: Wait for the dialogue cooldown
+            float elapsed = 0f;
+            while (elapsed < dialogue.dialogueviewtime)
+            {
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            // Step 2: Enable mouse icon for click prompt
+            CutsceneMouseIcon.SetActive(true);
+
+            // Step 3: Wait for player click to advance
+            clicked = false;
+            while (!clicked)
+            {
+                if (Input.GetMouseButtonDown(0))
+                    clicked = true;
+                yield return null;
+            }
+
+            // Disable the icon immediately after clicking
+            CutsceneMouseIcon.SetActive(false);
+        }
+
+        dialogueImage.gameObject.SetActive(false);
+        dialogueImage.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        yield return new WaitForSeconds(mainSlideTime);
+        clicked = false;
+        CutsceneMouseIcon.SetActive(true);
+        // while (!clicked)
+        // {
+        //     if (Input.GetMouseButtonDown(0))
+        //         clicked = true;
+        //     yield return null;
+        // }
+        
+        timeout = false;
+        firstTime = true;
+
+        currentSprite++;
+        isPlayingDialogue = false;
+    }
+
+
+
+
+
+
+private IEnumerator AdvanceScene(int time)
+    {
+        // Block clicks until timeout ends
+        timeout = true;
+        CutsceneMouseIcon.SetActive(false);
+
+        yield return new WaitForSeconds(time);
+
+        timeout = false;
+
+        currentSprite++; // move to next slide
+    }
 }
-
-
-
 
 [System.Serializable]
 public class SpriteRecord
 {
+    [System.Serializable]
+    public class Dialogue
+    {
+        public Sprite dialogueSprite;
+        public int dialogueviewtime;
+    }
     public Sprite sprite;
     public int viewtime;
-    
+    public bool hasDialogue;
+    public List<Dialogue> dialogues;
 }
